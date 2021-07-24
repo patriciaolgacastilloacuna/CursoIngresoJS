a263bdd94622dd97325bff6bdf2d6307e25d3b1d
patriciaolgacastilloacuna <86858569+patriciaolgacastilloacuna@users.noreply.github.com>
2021-07-23 23:49:54 -0300

    subo tp

--- 8-TPs/01-jsFerreteFacturacion.js
+++ 8-TPs/01-jsFerreteFacturacion.js
@@ -5,13 +5,57 @@ C.	ingresar tres precios de productos  y mostrar precio final (más IVA 21%).
 */
 function Sumar () 
 {
-	
+	let precioUno;
+    let precioDos;
+    let precioTres;
+
+    precioUno = parseInt(document.getElementById("txtIdPrecioUno").value);
+    precioDos = parseInt(document.getElementById("txtIdPrecioDos").value);
+    precioTres = parseInt(document.getElementById("txtIdPrecioTres").value);
+    resultado = precioUno + precioDos + precioTres;
+
+    alert ("el subtotal de sus productos es $" + resultado);
+
+
 }
 function Promedio () 
+
+    let precioUno;
+    let precioDos;
+    let precioTres;
+    let promedio;
+
+    precioUno = parseInt(document.getElementById("txtIdPrecioUno").value);
+    precioDos = parseInt(document.getElementById("txtIdPrecioDos").value);
+    precioTres = parseInt(document.getElementById("txtIdPrecioTres").value);
+    promedio = (precioUno + precioDos + precioTres) / 3;
+    alert("el promedio es" + promedio);
+
+
+
+
+
+
 {
 	
 }
 function PrecioFinal () 
 {
 	
+    let precioUno;
+    let precioDos;
+    let precioTres;
+    let subtotalPrecios;
+    let resultado
+
+    precioUno = parseInt(document.getElementById("txtIdPrecioUno").value);
+    precioDos = parseInt(document.getElementById("txtIdPrecioDos").value);
+    precioTres = parseInt(document.getElementById("txtIdPrecioTres").value);
+    subtotalPrcios = precioUno + precioDos + precioTres;
+    resultado = subtotalPrecios + subtotalPrecios * 0.21;
+    alert("el total de sus productos es $" + resultado);
+
+
+
+
 }
