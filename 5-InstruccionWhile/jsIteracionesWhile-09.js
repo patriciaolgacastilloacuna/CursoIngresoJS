731b55074064a4ed0f75b337bebfa9958932af58
patriciaolgacastilloacuna <86858569+patriciaolgacastilloacuna@users.noreply.github.com>
2021-08-04 18:14:51 -0300

    Add files via upload

--- 5-InstruccionWhile/jsIteracionesWhile-09.js
+++ 5-InstruccionWhile/jsIteracionesWhile-09.js
@@ -1,21 +1,42 @@
 /*
 Al presionar el botón pedir  números  hasta que el usuario quiera,
 mostrar el número máximo y el número mínimo ingresado.*/
-function mostrar()
-{	// declarar variables
-	var banderaDelPrimero;
-	var numeroIngresado;
-	var numeroMaximo;
-	var numeroMinimo;
-	var respuesta;
-	//iniciar variables
-	banderaDelPrimero="es el primero";
-	respuesta='si';
-	while(respuesta=="si")
-	{
-		
-		respuesta=prompt("desea continuar?");
+function mostrar(){	
+	let banderaDelPrimero =1;
+	let numeroIngresado;
+	let numeroMaximo;
+	let numeroMinimo;
+	let respuesta = "si";
+	
+	do{
+		numeroIngresado = parseInt(prompt("Ingrese un numero"));
+      
+	    while(isNaN(numeroIngresado)){
+			numeroIngresado = parseInt(prompt("Eso no es un numero. Por favor ingrese un numero"));
+		} 
+		if( banderaDelPrimero == 1){
+			numeroMaximo = numeroIngresado;
+			numeroMinimo = numeroIngresado;
+		    banderaDelPrimero = 0;
+		}
+	
+	     else if(numeroIngresado > numeroMaximo){
+		numeroMaximo = numeroIngresado;
 	}
-	txtIdMaximo.value=numeroMaximo;
-	txtIdMinimmo.value=numeroMinimo;
-}//FIN DE LA FUNCIÓN
+	      else{if(numeroIngresado < numeroMinimo){
+          numeroMinimo = numeroIngresado;
+	      }
+	      }
+	
+		respuesta = prompt("Desea seguir ingresando numeros? si/no");
+
+		while(respuesta != "si" && respuesta != "no"){
+			alert("Valor incorrecto. responda correctamente");
+			respuesta = prompt("Desea seguir ingresando numeros? si/no");
+		}
+
+	}while(respuesta != "no");
+
+document.getElementById("txtIdMaximo").value = numeroMaximo;
+document.getElementById("txtIdMinimo").value = numeroMinimo;
+}
--- 5-InstruccionWhile/jsIteracionesWhile-10.js
+++ 5-InstruccionWhile/jsIteracionesWhile-10.js
@@ -13,17 +13,68 @@ hasta que el usuario quiera, mostrar:
 function mostrar()
 {
 	//declarar contadores y variables 
-	var respuesta;
-	var numeroIngresado;
-	var sumaNegativos=0;
+	let respuesta;
+	let numero;
+	let contadorpositivo = 0;
+	let contadornegativo = 0;
+	let contadorcero = 0;
+	let contadorpares = 0;
+	let acumuladorpositivo = 0;
+	let acumuladornegativos = 0;
+	let promediopositivo = 0;
+	let promedionegativo = 0;
+	let diferencia;
 
-	respuesta="si";
-
-	while(respuesta=="si")
+	do 
 	{
-		
-		respuesta=prompt("desea continuar?");
-	}//fin del while
+		numero = parseInt(prompt("Ingrese un numero"));
 
-	document.write("la suma de negativos es :"+sumaNegativos);
+		if(numero < 0)
+		{
+			acumuladornegativos = acumuladornegativos + numero;
+			contadornegativo = contadornegativo + 1;
+		}
+		else
+		{
+			if(numero == 0)
+			{
+				contadorcero = contadorcero + 1;
+			}
+			else
+			{
+				acumuladorpositivo = acumuladorpositivo + numero;
+				contadorpositivo = contadorpositivo + 1;
+			}
+		}
+
+		if(numero % 2 == 0)
+		{
+			contadorpares = contadorpares + 1;
+		}
+
+		respuesta = prompt("¿Quiere ingresar otro numero? (si/no)");
+
+	} while (respuesta == "si");
+
+	if(contadorpositivo > 0)
+	{
+		promediopositivo = acumuladorpositivo / contadorpositivo;
+	}
+	if(contadornegativo > 0)
+	{
+		promedionegativo = acumuladornegativos / contadornegativo;
+	}
+
+	diferencia = contadorpositivo - contadornegativo;
+
+	console.log("1-Suma de los negativos: " + acumuladornegativos);
+	console.log("2-Suma de los positivos: " + acumuladorpositivo);
+	console.log("3-Cantidad de positivos: " + contadorpositivo);
+	console.log("4-Cantidad de negativos: " + contadornegativo);
+	console.log("5-Cantidad de ceros: " + contadorcero);
+	console.log("6-Cantidad de números pares: " + contadorpares);
+	console.log("7-Promedio de positivos: " + promediopositivo);
+	console.log("8-Promedios de negativos: " + promedionegativo);
+	console.log("9-Diferencia: " + diferencia);
+
 }//FIN DE LA FUNCIÓN
